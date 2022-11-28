2abb39a45bf7b19da7848c35833260d58cf8f676
Miłosz Zięba <milosz.zieba@gmail.com>
2022-11-28 21:47:52 +0100

    ZigzagConversion

---
+++ LeetCode.Tests/0006.ZigzagConversion.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using LeetCode.Extensions;
+using LeetCode.Models;
+using System.Collections;
+
+namespace LeetCode.Tests
+{
+    public class ZigzagConversionTests
+    {
+
+        [Theory]
+        [ClassData(typeof(ZigzagConversionTestData))]
+        public void Convert(string s, int numRows, string expectedResult)
+        {
+            var zigzagConversion = new ZigzagConversion();
+            var result = zigzagConversion.Convert(s, numRows);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+
+    public class ZigzagConversionTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                "",
+                5,
+                ""
+            };
+            yield return new object[] {
+                null,
+                123,
+                ""
+            };
+            yield return new object[] {
+                "a",
+                123,
+                "a"
+            };
+            yield return new object[] {
+                "ab",
+                123,
+                "ab"
+            };
+            yield return new object[] {
+                "abc",
+                2,
+                "acb"
+            };
+            yield return new object[] {
+                "abcd",
+                1,
+                "abcd"
+            };
+            yield return new object[] {
+                "abcd",
+                2,
+                "acbd"
+            };
+            yield return new object[] {
+                "abcd",
+                3,
+                "abdc"
+            };
+            yield return new object[] {
+                "PAYPALISHIRING",
+                3,
+                "PAHNAPLSIIGYIR"
+            };
+            yield return new object[] {
+                "PAYPALISHIRING",
+                4,
+                "PINALSIGYAHRPI"
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LeetCode/0006.ZigzagConversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ZigzagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (String.IsNullOrEmpty(s))
+                return "";
+            if (s.Length <= 2 || numRows < 2)
+                return s;
+
+            var result = new char[s.Length];
+            short index = 0;
+            short cycleLength = (short)(2 * numRows - 2);
+            for (short i = 0; i < numRows; i++)
+            {
+                if (i == 0 || i == numRows - 1)
+                    for (short j = i; j < s.Length; j += cycleLength)
+                        result[index++] = s[j];
+                else
+                    for (short j = i; j < s.Length; j += cycleLength)
+                    {
+                        result[index++] = s[j];
+                        if (j + cycleLength - 2 * i < s.Length)
+                            result[index++] = s[j + cycleLength - 2 * i];
+                    }
+            }
+            return new string(result);
+        }
+    }
+}
