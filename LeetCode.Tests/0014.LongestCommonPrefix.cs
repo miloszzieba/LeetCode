ecca181d767798bafff4e603e9ee472787a918bf
Milosz Zieba <milosz.zieba@gmail.com>
2022-12-10 01:25:35 +0100

    Longest Common Prefix

---
+++ LeetCode.Tests/0014.LongestCommonPrefix.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using LeetCode.Extensions;
+using LeetCode.Models;
+using System.Collections;
+
+namespace LeetCode.Tests
+{
+    public class _0014_LongestCommonPrefix
+    {
+        [Theory]
+        [ClassData(typeof(LongestCommonPrefixTestData))]
+        public void Soluton(string[] strs, string expectedResult)
+        {
+            var longestCommonPrefix = new LongestCommonPrefix();
+            var result = longestCommonPrefix.Solution(strs);
+            result.Should().Be(expectedResult);
+        }
+    }
+
+    public class LongestCommonPrefixTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new string[]{"Flower", "Flow", "Flight"},
+                "Fl"
+            };
+            yield return new object[] {
+                new string[] {"Dog", "Racecar", "Car"},
+                ""
+            };
+            yield return new object[] {
+                new string[0],
+                ""
+            };
+            yield return new object[] {
+                new string[]{ "Wahahaha" },
+                "Wahahaha"
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LeetCode/0014.LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestCommonPrefix
+    {
+        public string Solution(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+            if (strs.Length == 1)
+                return strs[0];
+
+            int longest = strs[0].Length;
+            for (int i = 1; i < strs.Length; i++)
+            {
+
+                if (longest > strs[i].Length)
+                    longest = strs[i].Length;
+                for (int j = 0; j < longest; j++)
+                    if (strs[0][j] != strs[i][j])
+                    {
+                        longest = j;
+                        break;
+                    }
+                if (longest == 0)
+                    return String.Empty;
+            }
+            return strs[0].Substring(0, longest);
+        }
+    }
+}
