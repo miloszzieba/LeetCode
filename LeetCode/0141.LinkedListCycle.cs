f3cce49cd7382b7f99573a197c529091225ba446
Miłosz Zięba <milosz.zieba@gmail.com>
2025-12-16 23:10:55 +0100

    LinkedListCycle, MissingNumber

---
+++ LeetCode/0141.LinkedListCycle.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LinkedListCycle
+    {
+        public bool Hashset(ListNode head)
+        {
+            var hashset = new HashSet<ListNode>();
+            while (head != null)
+            {
+                if (hashset.Contains(head))
+                    return true;
+
+                hashset.Add(head);
+                head = head.Next;
+            }
+
+            return false;
+        }
+
+        public bool TwoPointer(ListNode head)
+        {
+            if (head == null || head.Next == null) return false;
+
+            var fastPointer = head;
+
+            while (fastPointer != null && fastPointer.Next != null)
+            {
+                head = head.Next;
+                fastPointer = fastPointer.Next.Next;
+                if (head == fastPointer)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/0268.MissingNumber.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MissingNumber
+    {
+        public int Sum(int[] nums)
+        {
+            var sumTotal = (nums.Length * (nums.Length + 1)) / 2;
+            var sumMissing = 0;
+            for (int i = 0; i < nums.Length; i++)
+                sumMissing += nums[i];
+
+            return sumTotal - sumMissing;
+        }
+    }
+}
